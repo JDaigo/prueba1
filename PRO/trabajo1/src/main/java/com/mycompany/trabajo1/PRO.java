da3e0af9565d21f25c8f2cd66c44541bf1eafe53
Diego <jose.cerrato1@educa.madrid.org>
2023-11-17 00:04:04 +0100

    prueba

---
+++ PRO/trabajo1/src/main/java/com/mycompany/trabajo1/PRO.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.trabajo1;
+//en programacion estamos aprendiendo en java ,trabajando con if ,for ,do while,etc
+public class PRO {
+
+    public static void main(String[] args) {
+        System.out.println("Hola profe");
+        
+    }
+}
